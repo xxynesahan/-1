ea1cb8328ce8d22dab2021ba4e592e8c7e82a40e
xxynesahan <yuliya.yudina.2005@mail.ru>
2024-03-29 01:04:42 +0700

    milpops

---
+++ lababab.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float widht, hight, area, perimetr, diagonal;
+	// Ввод данных
+	cout << "Введите ширину прямоугольника" << endl;
+	cin >> widht;
+	cout << "Введите длину прямоугольника" << endl;
+	cin >> hight;
+	area = widht * hight; // Нахoждение площади. Формула S =  a * b
+	perimetr = 2 * (widht + hight); // Нахождение периметра. Формула P = 2 * (a + b)
+	diagonal = sqrt(widht * widht + hight * hight); // Нахождение диагонали. Формула  c = sqrt(a^2 + b^2)
+	cout << "Площадь прямоугольника" << " " << area << endl;
+	cout << "Периметр прямоугольника" << " " << perimetr << endl;
+	cout << "Длина диагонали" << " " << diagonal << endl;
+	return 0;
+}
